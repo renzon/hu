f687aad38e79facbba21353c1c9c20cd2d23b57a
renzon <renzon@gmail.com>
2016-05-13 17:04:03 -0300

    Trem como procolo informal

---
+++ huaula/trem.py
@@ -0,0 +1,13 @@
+class Trem(object):
+    def __init__(self, num_vagoes):
+        self.num_vagoes = num_vagoes
+
+    def __getitem__(self, pos):
+        indice = pos if pos >= 0 else self.num_vagoes + pos
+        if 0 <= indice < self.num_vagoes:  # indice 2 -> vagao #3
+            return 'vagao #%s' % (indice + 1)
+        raise IndexError('vagao inexistente [%s]' % pos)
+
+
+for vagao in Trem(4):
+    print(vagao)
